0921c9d0a1465e3c0c81af3e27596726a4c798b1
mehranredrose <mehranredrose@gmail.com>
2023-08-03 22:32:19 +0330

    create UserSW model in safe_word/model.py

--- Sword/safe_word/models.py
+++ Sword/safe_word/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+
+class UserSW(models.Model):
+    PW_TYPES = [('confidential', 'c'),('sharable', 's')]
+    title = models.CharField(max_length=100)
+    password = models.CharField(max_length=50)
+    date = models.DateTimeField(auto_now_add=True)
+    type = models.CharField(choices=PW_TYPES, max_length=12)
+    user = models.ForeignKey(User, on_delete=models.PROTECT, null=False, blank=False)
+    
+    def __str__(self):
+        return self.title
