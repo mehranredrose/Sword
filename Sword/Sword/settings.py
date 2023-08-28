b9bcc57a891ea1ca37b2d844f05b2e0150a52a14
mehranredrose <mehranredrose@gmail.com>
2023-08-28 10:37:31 +0330

    add CustomUser config in setting

--- Sword/Sword/settings.py
+++ Sword/Sword/settings.py
@@ -106,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
-AUTH_USER_MODEL = 'users.CustomUser'
+AUTH_USER_MODEL = 'safe_word.CustomUser'
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
