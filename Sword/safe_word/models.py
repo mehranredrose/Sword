d2bf9c59a3b86a930fcb6aceb1529a2dedb311eb
mehranredrose <mehranredrose@gmail.com>
2023-08-28 10:28:27 +0330

    changed UserSW user foreignkey into CustomUser in models

--- Sword/safe_word/models.py
+++ Sword/safe_word/models.py
@@ -10,7 +10,7 @@ class UserSW(models.Model):
     password = models.CharField(max_length=50)
     date = models.DateTimeField(auto_now_add=True)
     type = models.CharField(choices=PW_TYPES, max_length=12)
-    user = models.ForeignKey(User, on_delete=models.PROTECT, null=False, blank=False)
+    user = models.ForeignKey(CustomUser, on_delete=models.PROTECT, null=False, blank=False)
     
     def __str__(self):
         return self.title
